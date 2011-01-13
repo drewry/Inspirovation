7035ef36c2d5d190c26fcf641fd8e35ebc1314b6
Drew Morris <dhmorris@gmail.com>
2011-01-13 00:08:36 -0600

    Getting rid of the config value

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,5 @@ Inspirovation::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  heroku config:add BUNDLE_WITHOUT="development:test"
+ 
 end
