d9e1e5167cd6cddc6a2a8c21e5493cb4a7f075ef
Drew Morris <dhmorris@gmail.com>
2011-01-13 00:41:14 -0600

    Adding Sha1 encryption

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -43,6 +43,8 @@ Devise.setup do |config|
   # from others authentication tools as :clearance_sha1, :authlogic_sha512 (then
   # you should set stretches above to 20 for default behavior) and :restful_authentication_sha1
   # (then you should set stretches to 10, and copy REST_AUTH_SITE_KEY to pepper)
+  
+  config.encryptor = :sha1
   #config.encryptor = :bcrypt
 
   # Setup a pepper to generate the encrypted password.
